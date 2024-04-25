7f75a7d7e38292dda35759510ce2e41be8973b71
Alexis Haldy <alexishaldy@gmail.com>
2024-04-25 11:06:53 +0200

    Add text to tell which player we are and commands

--- client/include/Renderer/Renderers/GameRenderer.h
+++ client/include/Renderer/Renderers/GameRenderer.h
@@ -16,10 +16,23 @@ class GameRenderer final :
 	ScreenSizeValue _height;
 	ScreenSizeValue _width;
 
+	static constexpr short LINES_COUNT = 2;
+	static constexpr std::string_view START_PLAYER_MESSAGE_LINE_0 = "Evade and climb on bricks !";
+	static constexpr std::string_view START_PLAYER_MESSAGE_LINE_1 = "Use WASD to move";
+	static constexpr std::string_view START_GHOST_MESSAGE_LINE_0 = "Crush the player under bricks !";
+	static constexpr std::string_view START_GHOST_MESSAGE_LINE_1 = "Use AD to move and S to spawn bricks";
+	const sf::Color START_COLOR = sf::Color::Cyan;
+	const sf::Color END_COLOR = sf::Color::White;
+
+	static constexpr float MESSAGE_TIMER = 6.f;
+	float _messageTimer = 0;
+
 	bool _gameOver = false;
 
 	void OnDraw(sf::RenderTarget& target, sf::RenderStates states) const override;
 	void OnInput(sf::Event event) override;
+
+	static sf::Color LerpColor(sf::Color a, sf::Color b, float t) ;
 
  public:
 	explicit GameRenderer(Application& game, GameManager& gameManager, ScreenSizeValue width, ScreenSizeValue height);
--- client/src/Renderer/Renderers/GameRenderer.cpp
+++ client/src/Renderer/Renderers/GameRenderer.cpp
@@ -6,7 +6,34 @@
 GameRenderer::GameRenderer(Application& game, GameManager& gameManager, ScreenSizeValue width, ScreenSizeValue height) :
 	_application(game), _gameManager(gameManager), _height(height), _width(width)
 {
-	//TODO: Tell "You are the player" / "You need to stomp the player" with a timer going to 0 given by gameManager/decreasing from it
+	std::array<std::string_view, LINES_COUNT> lines;
+
+	if (_gameManager.GetPlayerRole() == PlayerRole::PLAYER)
+	{
+		lines = { START_PLAYER_MESSAGE_LINE_0, START_PLAYER_MESSAGE_LINE_1 };
+	}
+	else
+	{
+		lines = { START_GHOST_MESSAGE_LINE_0, START_GHOST_MESSAGE_LINE_1 };
+	}
+
+	_texts.push_back(Text(
+		sf::Vector2f(_width.Value / 2.f, _height.Value / 2.f),
+		{
+			TextLine({ CustomText{
+				.Text = lines[0].data(),
+			    .Color = sf::Color::Cyan,
+			    .Size = 30,
+			}}),
+			TextLine({ CustomText{
+				.Text = lines[1].data(),
+			    .Color = sf::Color::Cyan,
+			    .Size = 25,
+			}})
+		}
+	));
+
+	_messageTimer = MESSAGE_TIMER;
 }
 
 void GameRenderer::OnDraw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -66,6 +93,18 @@ void GameRenderer::OnFixedUpdate(sf::Time elapsed) {}
 void GameRenderer::OnUpdate(sf::Time elapsed, sf::Time elapsedSinceLastFixed, sf::Vector2f mousePosition)
 {
 	_gameManager.UpdatePlayerAnimations(elapsed, elapsedSinceLastFixed);
+
+	if (_messageTimer == 0.f) return;
+
+	_messageTimer -= elapsed.asSeconds();
+
+	_texts[0].SetColor(LerpColor(START_COLOR, END_COLOR, 1.f - _messageTimer / MESSAGE_TIMER));
+
+	if (_messageTimer <= 0.f)
+	{
+		_messageTimer = 0.f;
+		_texts.clear();
+	}
 }
 
 void GameRenderer::OnEvent(Event event)
@@ -80,4 +119,14 @@ void GameRenderer::OnEvent(Event event)
 			TextLine({ CustomText{ .Text = "Other player leave", .Size = 30 }})
 		}
 	);
+}
+
+sf::Color GameRenderer::LerpColor(sf::Color a, sf::Color b, float t)
+{
+	return {
+		static_cast<sf::Uint8>(a.r + t * (b.r - a.r)),
+		static_cast<sf::Uint8>(a.g + t * (b.g - a.g)),
+		static_cast<sf::Uint8>(a.b + t * (b.b - a.b)),
+		static_cast<sf::Uint8>(a.a + t * (b.a - a.a))
+	};
 }
